74fe11c947a0fc0afc4d9e5f55ea405c4d9c77be
HanhHuynh <huuhanh311001@gmail.com>
2022-04-25 14:43:35 +0700

    add validate.js

---
+++ server/middlewares/validateAdmin.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+const verifyToken = (req, res, next) => {
+    
+    const token = req.body.token || req.query.token || req.headers["x-access-token"];
+
+    if (!token) {
+        return res.status(400).json("A token is required for authentication");
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET);
+        req.user = decoded;
+    } catch (err) {
+        return res.status(403).json("Invalid Token");
+    }
+
+    return (req.user.permission !== 'admin') 
+        ? res.status(403).json({ success: 0, msg: 'Unauthorized' })
+        : next();
+};
+
+module.exports = verifyToken;
